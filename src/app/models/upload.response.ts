eab83daff1859b0e59f8ec9ee5bfb64fe0cc9f6b
Mohamed <mohamed@deepscenario.com>
2025-01-11 21:28:50 +0100

    Use endpoints instead of mocks and add support for saving the protocol in the backend database. Add

--- src/app/models/upload.response.ts
+++ src/app/models/upload.response.ts
@@ -1,4 +1,10 @@
 export interface UploadResponse {
   id: string;
+}
+
+export interface SpeakerResponse {
+  percentage: number;
+  isAnnotationDone: boolean;
+  isDone: boolean;
   persons: Record<string, string>;
 }
